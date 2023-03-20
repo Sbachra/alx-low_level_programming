588ac8e9c3431b639867355f91b5984fafc5d94f
Sbachra <bachrasoufiane@gmail.com>
2023-03-20 16:34:15 -0700

    hello world c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,15 @@
+#include <unistd.h>
+/**
+ * main - a C program that prints a line
+ * Use the function puts and printf are not allowed
+ * You are not allowed to use printf
+ * Return: 1
+ */
+int main(void)
+{
+	char line[] = "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n";
 
+	write(STDERR_FILENO, line, sizeof(line) - 1);
+
+	return (1);
+}
