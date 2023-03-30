4a2c8ffe73dd95a2dda215369f045a11a54048e0
Sbachra <bachrasoufiane@gmail.com>
2023-03-30 14:06:41 -0700

    C project 6

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -1,27 +1,25 @@
 #include "main.h"
 
 /**
- * _strncat - concatenates two strings.
+ * _strcat - concatenates two strings.
  *
  * @src: input string.
  *
  * @dest: overwrited string.
  *
- * @n: number of bytes from src.
- *
  * Return: Always 0.
  */
 
-char *_strncat(char *dest, char *src, int n)
+char *_strcat(char *dest, char *src)
 {
 	int	i;
 	int	dest_size;
 
 	i = 0;
 	dest_size = 0;
-	while (dest[dest_size] != '\0')
+	while (dest[dest_size])
 		dest_size++;
-	while (i < n && src[i] != '\0')
+	while (src[i])
 	{
 		dest[dest_size] = src[i];
 		dest_size++;
